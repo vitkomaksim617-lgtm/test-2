14f443acb4f1c7a4581829da2ba32ad9be0eefd5
Maksym <vitkomaksim617@gmail.com>
2025-12-14 12:50:37 +0200

    Delete 8 directory

--- 8/js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-	menu.classList.toggle('menu--open')
-});
