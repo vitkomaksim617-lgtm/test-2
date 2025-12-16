8bd475a6316e2fa317a932b845c6858dc0f07167
Maksym <vitkomaksim617@gmail.com>
2025-12-16 08:57:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,33 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
 	menu.classList.toggle('menu--open')
 });
+
+const mainImage = document.getElementById('mainImage');
+const galleryThumbs = document.getElementById('galleryThumbs');
+
+galleryThumbs.addEventListener('click', (e) => {
+	if (e.target.tagName !== 'IMG') return;
+	const clickedThumb = e.target;
+
+	const currentMainSrc = mainImage.src;
+
+	mainImage.src = clickedThumb.src;
+
+	clickedThumb.src = currentMainSrc;
+
+	galleryThumbs.appendChild(clickedThumb);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 'auto',
+		spaceBetween: 0,
+		grabCursor: true,
+		loop: true,
+		centeredSlides: false,
+		freeMode: true,
+	});
+});
+
+
